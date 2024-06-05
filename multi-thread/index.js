a837f3389b56020de78b3ff82dd22f2824d18ed0
Your Name <you@example.com>
2024-06-05 14:11:40 +0300

    fix issue

--- multi-thread/index.js
+++ multi-thread/index.js
@@ -1,10 +1,7 @@
 const { Worker } = require('worker_threads');
 const perf_hooks = require('perf_hooks');
-const OS = require('os');
 
-process.env.UV_THREADPOOL_SIZE = OS.cpus().length;
-
-console.log(OS.cpus().length)
+const arr = Array.from({length: 30000}, (_, i) => i + 1); 
 
 const performanceObserver = new perf_hooks.PerformanceObserver((items, observer) => {
 	items.getEntries().forEach((entry) => {
@@ -19,12 +16,6 @@ const performanceObserver = new perf_hooks.PerformanceObserver((items, observer)
 
 performanceObserver.observe({ entryTypes: ['measure', 'function'] });
 
-
-const arr = [];
-for(let i = 1; i <= 300000; i++) {
-    arr.push(i);
-}
-
 const linear = (arr) => {
     performance.mark('linear start');
     let cnt = 0;
@@ -38,7 +29,7 @@ const linear = (arr) => {
     performance.measure('linear', 'linear start', 'linear end');
 }
 
-const count = (arr) => {
+const workerThread = (arr) => {
     return new Promise((resolve, reject) => {
         const worker = new Worker('./worker.js', {
             workerData: arr,
@@ -54,16 +45,27 @@ const count = (arr) => {
     });
 }
 
+function splitArray(arr) {
+    const chunkSize = Math.ceil(arr.length / 4);
+    const splitedArray = [];
+
+    for (let i = 0; i < 4; i++) {
+        const start = i * chunkSize;
+        const end = Math.min(start + chunkSize, arr.length);
+        const chunk = arr.slice(start, end);
+        splitedArray.push(chunk);
+    }
+    return splitedArray;
+}
+
 const paralel = async (arr) => {
     performance.mark('paralel start');
-    let cnt = 0;
+    
+    const splitedArray = splitArray(arr);
+    const req = splitedArray.map(arr => workerThread(arr));
+    const res = await Promise.all(req);
+    console.log(res.reduce((acc, item) => acc + item));
 
-    const cnt_1 = await count(arr.slice(0, 75000));
-    const cnt_2 = await count(arr.slice(75000, 150000));
-    const cnt_3 = await count(arr.slice(150000, 225000));
-    const cnt_4 = await count(arr.slice(225000, 300000));
-
-    console.log(cnt_1+cnt_2+cnt_3+cnt_4);
     performance.mark('paralel end');
     performance.measure('paralel', 'paralel start', 'paralel end');
 }
