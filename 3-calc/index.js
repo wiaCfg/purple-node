a17eaab4e923775f88ae127134381c3c4794d172
Your Name <you@example.com>
2024-06-07 12:43:23 +0300

    fix comments

--- 3-calc/index.js
+++ 3-calc/index.js
@@ -4,14 +4,14 @@ const firstNum = process.argv[2];
 const secondNum = process.argv[3];
 const operation = process.argv[4];
 
-if(!fs.existsSync(`${operation}.js`) ||
-    !firstNum || 
-    !secondNum
-    ) {
+if(isNaN(firstNum) || isNaN(secondNum) || !isFinite(firstNum / secondNum)) {
     console.error('Error! Incorrect arguments passed!');
     return;
 }
 
-const calculator = require(`./${operation}.js`);
-
-console.log(calculator(parseFloat(firstNum), parseFloat(secondNum)));
+try {
+    const calculator = require(`./${operation}.js`);
+    console.log(calculator(parseFloat(firstNum), parseFloat(secondNum)));
+} catch (error) {
+    console.error('Error! Unknown calculator...')
+}
